8eaef43f3f6cdb511954af2261bbed4f34dc2dce
creator-ehsaas <creatorehsaas@gmail.com>
2021-07-13 20:00:30 +0530

    counter app react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {useState} from 'react';
+import "./style.css";
+const App=()=>{
+    
+    let [score, setScore] = useState(0) //setting default value for set with useState()
+    return(
+        <div>
+            <h1>Hey let's have some counting practice!</h1>
+            <div className="box"><h2>Counter: {score}</h2></div>
+            <button className="myButton" onClick={()=>(score >= 25)? "" : setScore(score+1)}>Increment</button> 
+            <button className="myButton" onClick={()=>{(score > 0)? setScore(score-1) : setScore(0)}}>Decrement</button>
+            <button className="myButton" onClick={()=>{setScore(0)}}>Reset</button>
+        </div>
+    )
 }
 
-export default App;
+export default App
